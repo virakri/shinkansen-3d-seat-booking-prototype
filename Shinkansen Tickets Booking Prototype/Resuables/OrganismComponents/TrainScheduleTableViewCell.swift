977f71181244cf464e8c9bfb9ea2656d4abd1112
virakri <v@mapraw.com>
2019-05-31 22:25:22 -0400

    add new card tableviewcell

---
+++ Shinkansen Tickets Booking Prototype/Resuables/OrganismComponents/TrainScheduleTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  TrainScheduleTableViewCell.swift
+//  Shinkansen Tickets Booking Prototype
+//
+//  Created by Virakri Jinangkul on 5/31/19.
+//  Copyright © 2019 Virakri Jinangkul. All rights reserved.
+//
+
+import Foundation
