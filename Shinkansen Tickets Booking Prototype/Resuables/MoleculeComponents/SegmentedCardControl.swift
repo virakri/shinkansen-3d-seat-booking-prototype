4aa759c8a0cda4bee78f5b299d3ad38db8a91e5f
virakri <v@mapraw.com>
2019-06-02 08:41:17 -0400

    Add new files

---
+++ Shinkansen Tickets Booking Prototype/Resuables/MoleculeComponents/SegmentedCardControl.swift
@@ -0,0 +1,13 @@
+//
+//  SegmentedCardControl.swift
+//  Shinkansen Tickets Booking Prototype
+//
+//  Created by Virakri Jinangkul on 6/2/19.
+//  Copyright © 2019 Virakri Jinangkul. All rights reserved.
+//
+
+import UIKit
+
+class SegmentedCardControl: UIControl {
+    
+}
